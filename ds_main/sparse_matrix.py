1b5473c1c4c1c53aa7c68ea94b340662fdebc112
matin <irealmatin@gmail.com>
2024-10-25 22:52:37 +0330

    sparse matrix class structure

--- ds_main/sparse_matrix.py
+++ ds_main/sparse_matrix.py
@@ -3,3 +3,22 @@
 #------------------------------------------------------------------------------------------------------#
 #-----------------------------------------------code---------------------------------------------------#
 #------------------------------------------------------------------------------------------------------#
+
+class SparseMatrix:
+    def __init__(self , rows , cols):
+        self.rows = rows
+        self.cols = cols
+        self.matrix = {}
+
+    def add_value(self , row , col , value):
+        if value != 0 :
+            self.matrix[(row , col)] = value
+
+    def multiply(self):
+        pass
+
+    def add(self):
+        pass
+    
+    def __str__(self):
+        pass
